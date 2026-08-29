5f5b4d00cacd74d9ec80f8db50e83f69948670c6
Sushmita jena <sushmitajena845@gmail.com>
2026-08-29 12:50:56 +0530

    Time: 0 ms (100%), Space: 97.2 MB (99.45%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int n = prices.size();
+        int minprice = prices[0];
+        int maxprofit = 0;
+
+        for(int i=1; i<n; i++){
+            minprice= min(minprice, prices[i]);
+
+            int profit= prices[i] - minprice;
+            maxprofit = max( maxprofit, profit);
+        }
+    return maxprofit;  
+    }
+    
+};
